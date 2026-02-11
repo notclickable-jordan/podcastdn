11a0b55eaf9822e03834952976217191dfc242bc
Jordan Roher <jordan@notclickable.com>
2026-02-11 12:44:40 -0800

    Fixed local login

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import { compare } from "bcryptjs";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: process.env.AUTH_TRUST_HOST === "true",
   pages: {
     signIn: "/login",
   },
